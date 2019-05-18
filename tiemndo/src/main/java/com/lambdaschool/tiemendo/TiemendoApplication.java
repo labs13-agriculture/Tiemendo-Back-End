bf92a8deaa4bb262ef3d2433d8d0f810bdfd3177
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-05-18 15:38:37 -0500

    Initial Commit

---
+++ tiemndo/src/main/java/com/lambdaschool/tiemendo/TiemendoApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.tiemendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiemendoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TiemendoApplication.class, args);
+    }
+
+}
