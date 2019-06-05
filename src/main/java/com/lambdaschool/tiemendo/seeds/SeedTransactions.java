ccd28e3b746f6340243e32e74fba99b6bf75b504
dgbailey <bailey.dust@gmail.com>
2019-06-05 16:40:29 -0700

    BUG: Merge conflicts resolved. Transaction Seed data functional.
    Repos, services, serviceimpls added for Transaction and TransactionItem

--- src/main/java/com/lambdaschool/tiemendo/seeds/SeedTransactions.java
+++ src/main/java/com/lambdaschool/tiemendo/seeds/SeedTransactions.java
@@ -20,11 +20,18 @@ public class SeedTransactions implements CommandLineRunner
     TransactionRepository transactionRepository;
     TransactionItemRepository transactionItemRepository;
     ItemTypeRepository itemTypeRepository;
+    RetailerRepository retailerRepository;
+    RetailerContactRepository retailerContactRepository;
+    RetailerLocationRepository retailerLocationRepository;
 
-    public SeedTransactions(TransactionRepository transactionRepository, TransactionItemRepository transactionItemRepository,ItemTypeRepository itemTypeRepository) {
+    public SeedTransactions(TransactionRepository transactionRepository, TransactionItemRepository transactionItemRepository,ItemTypeRepository itemTypeRepository,RetailerRepository retailerRepository,RetailerContactRepository retailerContactRepository,RetailerLocationRepository retailerLocationRepository) {
         this.transactionRepository = transactionRepository;
         this.transactionItemRepository = transactionItemRepository;
         this.itemTypeRepository = itemTypeRepository;
+        this.retailerRepository = retailerRepository;
+        this.retailerLocationRepository = retailerLocationRepository;
+        this.retailerContactRepository = retailerContactRepository;
+
 
     }
 
@@ -43,10 +50,19 @@ public class SeedTransactions implements CommandLineRunner
         TransactionItem ti2 = new TransactionItem(10,1.00,new ItemType(),new Transaction());
 
         //adding retailer
+        RetailerLocation rl1 = new RetailerLocation("123 main street", "North", "District 2", "Downtown", "Crooked tree", new Retailer());
+        RetailerContact rc1 = new RetailerContact("Mr", "John Doe", "M", "Ghanaian", "01/01/2010", "PhD", "Manager", "555-555-5555", "email@example.com", new Retailer());
+
+        Retailer r1 = new Retailer("Moes", false, 1989, rl1, rc1);
+
+        rc1.setRetailer(r1);
+        rl1.setRetailer(r1);
+
+
 
 
         //creating transaction object
-        Transaction t1 = new Transaction("Cash",new Date(), transactionitems, "Ronaldo Bean",);
+        Transaction t1 = new Transaction("Cash",new Date(), transactionitems, "Ronaldo Bean",r1);
 
 
         t1.getInputs().add(ti1);
@@ -60,7 +76,7 @@ public class SeedTransactions implements CommandLineRunner
 
 
 
-
+        retailerRepository.save(r1);
         itemTypeRepository.save(ii1);
         itemTypeRepository.save(ii2);
         transactionItemRepository.save(ti1);
