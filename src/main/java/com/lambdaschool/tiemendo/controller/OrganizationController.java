c8ff34c621935be9aef40658401c52f9ab6cc5d2
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-06-04 22:24:33 -0500

    a few Organization enpoints

--- src/main/java/com/lambdaschool/tiemendo/controller/OrganizationController.java
+++ src/main/java/com/lambdaschool/tiemendo/controller/OrganizationController.java
@@ -1,14 +1,18 @@
 package com.lambdaschool.tiemendo.controller;
 
 import com.lambdaschool.tiemendo.model.Organization;
+import com.lambdaschool.tiemendo.model.OrganizationContact;
 import com.lambdaschool.tiemendo.service.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -18,10 +22,31 @@ public class OrganizationController
     @Autowired
     private OrganizationService organizationService;
 
+
     @GetMapping(value = "organizations-list", produces = {"application/json"})
     public ResponseEntity<?> listAllOrganizations()
     {
         List<Organization> myOrganization = organizationService.findAll();
         return new ResponseEntity<>(myOrganization, HttpStatus.OK);
     }
+
+    @GetMapping(value = "/{organizationId}", produces = {"application/json"})
+    public ResponseEntity<?> getOrganizationById(@PathVariable Long organizationId)
+    {
+        Organization o = organizationService.findOrganizationById(organizationId);
+        return new ResponseEntity<>(o, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/new-organization", consumes = {"application/json"}, produces = {"application/json"})
+    public ResponseEntity<?> addNewOrganization(@Valid @RequestBody Organization newOrganization) throws URISyntaxException
+    {
+        newOrganization = organizationService.save(newOrganization);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newOrganizationURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{organizationid}").buildAndExpand(newOrganization).toUri();
+        responseHeaders.setLocation(newOrganizationURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+
 }
--- src/main/java/com/lambdaschool/tiemendo/service/OrganizationServiceImpl.java
+++ src/main/java/com/lambdaschool/tiemendo/service/OrganizationServiceImpl.java
@@ -2,10 +2,17 @@ package com.lambdaschool.tiemendo.service;
 
 
 import com.lambdaschool.tiemendo.model.Organization;
+import com.lambdaschool.tiemendo.model.OrganizationContact;
+import com.lambdaschool.tiemendo.model.OrganizationLocation;
+import com.lambdaschool.tiemendo.repository.OrganizationContactRepository;
+import com.lambdaschool.tiemendo.repository.OrganizationLocationRepository;
 import com.lambdaschool.tiemendo.repository.OrganizationRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +23,66 @@ public class OrganizationServiceImpl implements OrganizationService
     @Autowired
     private OrganizationRepository organizationRepos;
 
+    @Autowired
+    private OrganizationLocationRepository organizationLocationRepos;
+
+    @Autowired
+    private OrganizationContactRepository organizationContactRepos;
+
+
     @Override
     public List<Organization> findAll()
     {
         List<Organization> list = new ArrayList<>();
         organizationRepos.findAll().iterator().forEachRemaining(list::add);
         return list;
+    }
+
+
+    @Override
+    public Organization findOrganizationById(long id) throws EntityNotFoundException
+    {
+        return organizationRepos.findById(id).orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+    }
+
+    @Transactional
+    @Override
+    public Organization save(Organization organization)
+    {
+        Organization newOrganization = new Organization();
+
+        newOrganization.setBeneficiaries(organization.getBeneficiaries());
+        newOrganization.setHeadquarters(organization.getHeadquarters());
+
+        // ArrayList to hold new contacts
+        List<OrganizationContact> contactlist = new ArrayList<>();
+        organization.getOrganizationcontacts().iterator().forEachRemaining(c ->
+        {
+        c.setOrganization(newOrganization);
+        contactlist.add(c);
+        });
+
+        List<OrganizationLocation> locationlist = new ArrayList<>();
+        organization.getOrganizationlocations().iterator().forEachRemaining(l ->
+        {
+            l.setOrganization(newOrganization);
+            locationlist.add(l);
+        });
+
+        newOrganization.setOrganizationcontacts(organization.getOrganizationcontacts());
+        newOrganization.setOrganizationlocations(organization.getOrganizationlocations());
+
+
+        Organization organizationSaved = organizationRepos.save(newOrganization);
+        organizationContactRepos.saveAll(contactlist);
+        organizationLocationRepos.saveAll(locationlist);
+
+        return organizationSaved;
+    }
+
+    @Override
+    public Organization update(Organization organization, long id)
+    {
+        return null;
     }
 }
