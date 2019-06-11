cc10cbb98f4397850208aea1c150253156f6932f
KerstingJ <Kersting.Josh@Gmail.com>
2019-06-11 19:33:11 -0400

    added payment schedule fields to client

--- src/main/java/com/lambdaschool/tiemendo/model/Client.java
+++ src/main/java/com/lambdaschool/tiemendo/model/Client.java
@@ -3,7 +3,9 @@ package com.lambdaschool.tiemendo.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Entity
@@ -17,6 +19,18 @@ public abstract class Client extends Auditable {
     private String name;
     private boolean isLead;
     private String type;
+
+    // These are for payment schedules
+    private double paymentAmount;
+    private boolean isPayFrequencyDate;
+    private int paymentFrequency;
+    private LocalDate paymentStartDate;
+
+    @ElementCollection
+    @MapKeyColumn(name="due_date")
+    @Column(name="paid_date")
+    @CollectionTable(name="example_attributes", joinColumns=@JoinColumn(name="example_id"))
+    private HashMap<LocalDate, LocalDate> paymentSchedule = new HashMap<>();
 
     // Transactions
     @OneToMany(mappedBy="client", cascade = CascadeType.ALL)
@@ -88,4 +102,37 @@ public abstract class Client extends Auditable {
     public void setType(String type) {
         this.type = type;
     }
+
+    public double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(double paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public int getPaymentFrequency() {
+        return paymentFrequency;
+    }
+
+    public void setPaymentFrequency(int paymentFrequency) {
+        this.paymentFrequency = paymentFrequency;
+    }
+
+    public LocalDate getPaymentStartDate() {
+        return paymentStartDate;
+    }
+
+    public void setPaymentStartDate(LocalDate paymentStartDate) {
+        this.paymentStartDate = paymentStartDate;
+    }
+
+    public HashMap<LocalDate, LocalDate> getPaymentSchedule() {
+        return paymentSchedule;
+    }
+
+    public void setPaymentSchedule(HashMap<LocalDate, LocalDate> paymentSchedule) {
+        this.paymentSchedule = paymentSchedule;
+    }
+
 }
